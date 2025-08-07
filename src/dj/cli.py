3070fc8686fe53c5a9c15e7af7abd88dc45b2e6f
elad jerbi <elad.jerbi.dev@gmail.com>
2025-08-07 12:15:05 +0300

    correct cli description arguments to help

--- src/dj/cli.py
+++ src/dj/cli.py
@@ -15,10 +15,10 @@ def parser(prog_name: str) -> dict:
         "--version", action="version", version=f"%(prog)s {version(prog_name)}"
     )
     main_parser.add_argument(
-        "--s3prefix", type=str, description="S3 prefix for data storage"
+        "--s3prefix", type=str, help="S3 prefix for data storage"
     )
     main_parser.add_argument(
-        "--s3bucket", type=str, description="S3 bucket for data storage"
+        "--s3bucket", type=str, help="S3 bucket for data storage"
     )
     main_parser.add_argument("--s3endpoint", type=str, help="S3 endpoint URL")
     main_parser.add_argument(
@@ -26,23 +26,23 @@ def parser(prog_name: str) -> dict:
     )
     main_parser.add_argument("--echo", action="store_true", help="Echo SQL commands")
     main_parser.add_argument(
-        "--pool-size", type=int, description="Database connection pool size"
+        "--pool-size", type=int, help="Database connection pool size"
     )
     main_parser.add_argument(
-        "--max-overflow", type=int, description="Max overflow for database connections"
+        "--max-overflow", type=int, help="Max overflow for database connections"
     )
     main_parser.add_argument("--log-dir", type=str, help="Directory for log files")
     main_parser.add_argument(
         "--verbose",
         action="store_const",
         const=True,
-        description="Enable verbose logging",
+        help="Enable verbose logging",
     )
     main_parser.add_argument(
         "--plain",
         action="store_const",
         const=True,
-        description="Disable loading bar and colors",
+        help="Disable loading bar and colors",
     )
 
     # Subparsers
@@ -53,14 +53,14 @@ def parser(prog_name: str) -> dict:
         "config", help="configure dj settings."
     )
     config_parser.add_argument(
-        "--set-s3endpoint", type=str, description="Set S3 endpoint URL"
+        "--set-s3endpoint", type=str, help="Set S3 endpoint URL"
     )
-    config_parser.add_argument("--set-s3bucket", type=str, description="Set S3 bucket")
-    config_parser.add_argument("--set-s3prefix", type=str, description="Set S3 prefix")
+    config_parser.add_argument("--set-s3bucket", type=str, help="Set S3 bucket")
+    config_parser.add_argument("--set-s3prefix", type=str, help="Set S3 prefix")
     config_parser.add_argument(
         "--set-registry-endpoint",
         type=str,
-        description="Set registry (db) endpoint URL",
+        help="Set registry (db) endpoint URL",
     )
     config_parser.add_argument(
         "--set-echo", action="store_true", help="Enable SQL command echoing"
@@ -77,74 +77,74 @@ def parser(prog_name: str) -> dict:
         "load", help="load data into dj registry."
     )
     load_parser.add_argument(
-        "data_src", type=str, description="Source of data files (local or S3)"
+        "data_src", type=str, help="Source of data files (local or S3)"
     )
     load_parser.add_argument(
-        "dataset_name", type=str, description="Name of the dataset"
+        "dataset_name", type=str, help="Name of the dataset"
     )
-    load_parser.add_argument("--domain", type=str, description="Domain of the dataset")
+    load_parser.add_argument("--domain", type=str, help="Domain of the dataset")
     load_parser.add_argument(
         "--stage",
         choices=[stage.value for stage in DataStage],
-        description="Data stage",
+        help="Data stage",
     )
     load_parser.add_argument(
-        "--filters", nargs="+", description="Filters for data files"
+        "--filters", nargs="+", help="Filters for data files"
     )
     load_parser.add_argument(
         "--exists-ok",
         action="store_const",
         const=True,
-        description="Allow loading into existing datasets",
+        help="Allow loading into existing datasets",
     )
     load_parser.add_argument(
-        "--description", type=str, description="Description of the dataset"
+        "--description", type=str, help="Description of the dataset"
     )
-    load_parser.add_argument("--tags", nargs="+", description="Tags for the dataset")
+    load_parser.add_argument("--tags", nargs="+", help="Tags for the dataset")
 
     # Fetch
     fetch_parser: ArgumentParser = sub_parsers.add_parser(
         "fetch", help="fetch data from dj registry."
     )
     fetch_parser.add_argument(
-        "directory", type=str, description="Directory to save fetched files"
+        "directory", type=str, help="Directory to save fetched files"
     )
     fetch_parser.add_argument(
-        "limit", type=int, description="Limit the number of files to fetch"
+        "limit", type=int, help="Limit the number of files to fetch"
     )
-    fetch_parser.add_argument("--domain", type=str, description="Domain to filter by")
+    fetch_parser.add_argument("--domain", type=str, help="Domain to filter by")
     fetch_parser.add_argument(
-        "--dataset-name", type=str, description="Dataset name to filter by"
+        "--dataset-name", type=str, help="Dataset name to filter by"
     )
     fetch_parser.add_argument(
         "--stage",
         choices=[stage.value for stage in DataStage],
-        description="Data stage to filter by",
+        help="Data stage to filter by",
     )
-    fetch_parser.add_argument("--mime", type=str, description="MIME type to filter by")
-    fetch_parser.add_argument("--tags", nargs="+", description="Tags to filter by")
+    fetch_parser.add_argument("--mime", type=str, help="MIME type to filter by")
+    fetch_parser.add_argument("--tags", nargs="+", help="Tags to filter by")
     fetch_parser.add_argument(
-        "--sha256", nargs="+", description="SHA256 hashes to filter by"
+        "--sha256", nargs="+", help="SHA256 hashes to filter by"
     )
     fetch_parser.add_argument(
-        "--filenames", nargs="+", description="File names to filter by"
+        "--filenames", nargs="+", help="File names to filter by"
     )
     fetch_parser.add_argument(
         "--export-format",
         choices=EXPORT_FORMATS,
-        description="Format for exporting fetched data",
+        help="Format for exporting fetched data",
     )
     fetch_parser.add_argument(
         "--dry",
         action="store_const",
         const=True,
-        description="Dry run, do not actually download files",
+        help="Dry run, do not actually download files",
     )
     fetch_parser.add_argument(
         "--export",
         action="store_const",
         const=True,
-        description="Export fetched data to a file",
+        help="Export fetched data to a file",
     )
 
     return {k: v for k, v in vars(main_parser.parse_args()).items() if v is not None}
